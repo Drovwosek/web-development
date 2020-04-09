8832f870935ff3279d29eb090783d85a7725c133
Drovwosek <61798378+Drovwosek@users.noreply.github.com>
2020-04-09 22:19:44 +0300

    Update Task1.php

--- LW3/Task1.php
+++ LW3/Task1.php
@@ -1,6 +1,3 @@
 <?php
-  header("Content-Type: text/plain");
-  //проверить получение $text = $_GET['text']; а пока так
-  $text= 'ew';
-  echo preg_replace('/^ +| +$|( ) +/m', '$1', $text);
-?>
+$text = "    Помогите,    нас    разделяют   пробелы   ";
+echo trim($text);
