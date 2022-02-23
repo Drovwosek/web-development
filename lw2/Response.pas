08a76920aacc321cd43a854143eb451ef82d286c
Drovwosek <61798378+Drovwosek@users.noreply.github.com>
2022-02-24 00:09:58 +0300

    Update Response.pas

--- lw2/Response.pas
+++ lw2/Response.pas
@@ -4,11 +4,11 @@ USES
   DOS;
 
 BEGIN
-  WRITELN('Content-Type:text/plain');
+  WRITELN('Content-Type:text/plain');//формат передачи данных(текст)
   WRITELN;
-  WRITELN(GETENV('REQUEST_METHOD'));
-  WRITELN(GETENV('QUERY_STRING'));// строка со всеми переменными
-  WRITELN(GETENV('CONTENT_LENGTH'));
-  WRITELN(GETENV('HTTP_USER_AGENT'));
-  WRITELN(GETENV('HTTP_HOST'));
+  WRITELN(GETENV('REQUEST_METHOD'));// Запрашиваем метод запроса 
+  WRITELN(GETENV('QUERY_STRING'));// Строка со всеми переменными
+  WRITELN(GETENV('CONTENT_LENGTH'));// размер пересылаемого тела в байтах
+  WRITELN(GETENV('HTTP_USER_AGENT'));// строка с именем браузера, посылаемая в запросе
+  WRITELN(GETENV('HTTP_HOST'));// выкидывает инфу про машинку пользователя
 END.
