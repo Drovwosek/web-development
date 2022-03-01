d2b82301be2269d19c26b3f1a31460d6eb244da2
Drovwosek <artem-a-93@mail.ru>
2022-03-01 22:15:32 +0300

    3.1

--- LW3/Task1.php
+++ LW3/Task1.php
@@ -1,46 +1,5 @@
 <?php
-//$str = preg_replace('/\s+/', ' ', $str);
-//echo ($str);
-
-header("Content-Type: text/plain");
-$inputText = getQueryStringParameter('text');
-if ($inputText === '')
-{
-    echo 'Введена пустая строка';
-}
-elseif ($inputText !== null)
-{
-    echo removeExtraBlanks($inputText);
-}
-else
-{
-    echo 'Некорректный ввод данных';
-}
-
-function getQueryStringParameter(string $name): ?string
-{
-   return isset($_GET[$name]) ? $_GET[$name] : null;
-}
-
-function removeExtraBlanks(string $text): ?string
-{
-    $resultString = '';
-    $blank = true;
-    for ($i = 0; $i !== strlen($text); $i++) 
-    {
-        if ($text[$i] !== ' ')
-        {
-            $resultString .= $text[$i];
-            $blank = false;
-        }
-        else
-        {
-            if ($blank === false)
-            {
-                $blank = true;
-                $resultString .= ' ';
-            }
-        }
-    }
-    return $resultString;
-}
+$str = '  Margin     in Gooogle   is   8   px   ';
+$str = trim($str, '\s');
+$str = preg_replace('/\s\s+/', ' ', $str);
+echo $str;
