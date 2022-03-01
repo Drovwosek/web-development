478b2db8160b411c811c35fbd7e3609a8326f4ef
Drovwosek <61798378+Drovwosek@users.noreply.github.com>
2022-03-02 02:26:26 +0300

    Create Task2.php

---
+++ LW3/Task2.php
@@ -0,0 +1,40 @@
+<?php
+header("Content-Type: text/plain");
+
+function getQueryStringParameter(string $name): ?string
+{
+    return isset($_GET[$name]);  //? $_GET[$name] : null;
+}
+
+$inputText = getQueryStringParameter('identifier');
+
+if (strlen($inputText) > 0)
+{
+    checkIndentifier($inputText);
+}
+else
+{
+    echo 'incorrect INPUT';
+}
+
+function checkIndentifier(string $identifier)
+{
+    $hasErrors = false;
+    if (!is_numeric($identifier[0]))
+    {
+        for ($i = 0; $i < strlen($identifier); $i++)
+        {
+            if (!is_numeric($identifier[$i]) || !ctype_alpha($identifier[$i]))
+            {
+                echo 'it is cant be in identifier ' . "'" . $identifier[$i] . "'.\n";
+                $hasErrors = true;
+            }
+        }
+    }
+    else
+    {
+        echo "identifier cant start from number.\n";
+        $hasErrors = true;
+    }
+    echo $hasErrors ? 'no' : 'yes';
+}
