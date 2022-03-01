4938e3190ca4cb4c77679c2265910b4d72e58c6c
Drovwosek <61798378+Drovwosek@users.noreply.github.com>
2022-03-01 20:08:51 +0300

    Update Task1.php

--- LW3/Task1.php
+++ LW3/Task1.php
@@ -1,5 +1,47 @@
 <?php
-//$text = "    Помогите,    нас    разделяют   пробелы   ";
-//echo trim($text);
-$str = preg_replace('/\s+/', ' ', $str);
-echo ($str);
+//$str = preg_replace('/\s+/', ' ', $str);
+//echo ($str);
+
+<?php
+header("Content-Type: text/plain");
+$inputText = getQueryStringParameter('text');
+if ($inputText === '')
+{
+    echo 'Введена пустая строка';
+}
+elseif ($inputText !== null)
+{
+    echo removeExtraBlanks($inputText);
+}
+else
+{
+    echo 'Некорректный ввод данных';
+}
+
+function getQueryStringParameter(string $name): ?string
+{
+   return isset($_GET[$name]) ? $_GET[$name] : null;
+}
+
+function removeExtraBlanks(string $text): ?string
+{
+    $resultString = '';
+    $blank = true;
+    for ($i = 0; $i !== strlen($text); $i++) 
+    {
+        if ($text[$i] !== ' ')
+        {
+            $resultString .= $text[$i];
+            $blank = false;
+        }
+        else
+        {
+            if ($blank === false)
+            {
+                $blank = true;
+                $resultString .= ' ';
+            }
+        }
+    }
+    return $resultString;
+}
