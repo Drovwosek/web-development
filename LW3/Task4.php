a8f9680945160e0e0207f9b16feba91b1de77708
Drovwosek <61798378+Drovwosek@users.noreply.github.com>
2022-06-01 00:36:40 +0300

    Update Task4.php

--- LW3/Task4.php
+++ LW3/Task4.php
@@ -1,42 +1,39 @@
 <?php
-
-$_SERVER['QUERY_STRING'];
-$firstName = $_GET['first_name'];
-$lastName = $_GET['last_name'];
-$email = $_GET['email'];
-$age = $_GET['age'];
-
-$email .= ".txt";
-if (strlen($email) - 4 != 0)
+header("Content-Type: text/plain");
+function getQueryStringParameter(string $name): ?string
 {
-	if (file_exists($email))
-	{
-		$tempArray = file($email);
-		if (!(empty($firstName)))
-		{
-			$tempArray[0] = "First Name: " . $firstName . "\n";
-		}
-		if (!(empty($lastName)))
-		{
-			$tempArray[1] = "Last Name: " . $lastName . "\n";
-		}
-		if (!(empty($email)))
-		{
-			$tempArray[2] = "Email: " . substr($email, 0, -4) . "\n";
-		}
-		if (!(empty($age)))
-		{
-			$tempArray[3] = "Age: " . $age;
-		}
-		file_put_contents($email, $tempArray);
-	} else
-	{
-		$userTxt = fopen($email, "w");
-		fwrite($userTxt, "First Name: " . $firstName . "\n");
-		fwrite($userTxt, "Last Name: " . $lastName . "\n");
-		fwrite($userTxt, "Email: " . substr($email, 0, -4) . "\n");
-		fwrite($userTxt, "Age: " . $age);
-		fclose($userTxt);
-	}
+    return isset($_GET[$name]) ? $_GET[$name] : null;
 }
-?>
+
+$firstName = getQueryStringParameter('first_name');
+$lastName = getQueryStringParameter('last_name');
+$age = getQueryStringParameter('age');
+$email = getQueryStringParameter('email');
+
+if ($email)
+{
+    $path = "./data/" . $email . ".txt";
+    if (file_exists($path))
+    {
+        $myFile = fopen($path, "r");
+        getSubstringInFile(file($path));
+        fclose($myFile);  
+    }
+    $myFile = fopen($path, "w");
+    $somecontent = "First Name:" . $firstName . "\nLast Name:" . $lastName . "\nEmail:" . $email . "\nAge:" . $age;
+    fwrite($myFile, $somecontent);
+    fclose($myFile);
+}
+else
+{
+    echo 'have not email!'; 
+}
+
+function getSubstringInFile(array $arrayStrings): ?string
+{ 
+    global $firstName, $lastName, $age;
+    $firstName = $firstName ?? trim(substr($arrayStrings[0], strpos($arrayStrings[0], ':') + 1));
+    $lastName = $lastName ?? trim(substr($arrayStrings[1], strpos($arrayStrings[1], ':') + 1));
+    $age = $age ?? trim(substr($arrayStrings[3], strpos($arrayStrings[3], ':') + 1)); 
+    return '1';
+}
